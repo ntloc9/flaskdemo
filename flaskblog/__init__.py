801a08758050ba61d0a81f02da96e1b03cb962a0
Loc Nguyen <ntloc9@gmail.com>
2019-06-24 11:48:55 +0700

    update img profile and post

---
+++ flaskblog/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd07ffc88bd84d2d1d3e5db13e92a1ed7'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = u"Please login to access this page."
+login_manager.login_message_category = "info"
+
+
+
+from flaskblog import route
+
